f20ee58cd07b1d52525ab87f302ba2db3e6337b1
xiaokang00010 <zhouyongkang4321@gmail.com>
2025-07-19 13:58:43 +0800

    chores: add reference to original repo

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is also a middleware for CyberWaifu-v2, which wrapped the original Talking Head Anime 4 project into a powerful API and also implemented a simple animation system for different emotion expression and cache system.
 
 This API (`src/tha4_api`) would be licensed under MIT license.
+
+Original project: [Talking Head Anime 4 - by pkhungurn](https://github.com/pkhungurn/talking-head-anime-4-demo), licensed under MIT license.
--- src/tha4_api/test_animation.py
+++ src/tha4_api/test_animation.py
@@ -8,7 +8,7 @@ import torch
 manager = tha4_api.api.ImageInferenceManager(tha4_api.api.ImageInferenceManager.load_model(torch.device('cuda:0')), torch.device('cuda:0'))
 manager.set_base_image(PIL.Image.open('data/character_models/lambda_01/Layer 1_With_Mouth.png'))
 params = tha4_api.api.PoseUpdateParams()
-conf = animation.AnimationConfiguration(json.loads(pathlib.Path("./src/tha4_api/test_configuration.json").read_text()))
+conf = animation.AnimationConfiguration(json.loads(pathlib.Path("data/character_models/lambda_01/test_configuration.json").read_text()))
 renderer = animation.Renderer(conf, manager, 20)
 # print('\n\n'.join(i.__repr__() for i in renderer.compose_state('idle')))
 # renderer.deserialize('tha4_cache/Cyrene.tha4')
